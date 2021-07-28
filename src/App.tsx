d972202735cb9140bb92fb622297e91b81753db8
Yevhenii Ushakov <yevhenii.ushakov@mev.com>
2021-07-28 22:26:33 +0300

    Add tools

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Counter } from './Counter'
+import Button from '@material-ui/core/Button'
+
+export const App = () => {
+  return (
+    <>
+      <Button variant="contained">Default</Button>
+      <h1>React TypeScript Webpack Starter Template</h1>
+      <Counter />
+    </>
+  )
+}
